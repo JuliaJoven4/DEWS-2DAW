5b8e6c928bae3a9a1e075427a1e9e910378c1d5e
JuliaJoven4 <63252638+JuliaJoven4@users.noreply.github.com>
2021-11-04 23:56:10 +0100

    Update casino.php

--- Minicasino/casino.php
+++ Minicasino/casino.php
@@ -22,29 +22,42 @@ if ( ($_SERVER['REQUEST_METHOD'] == 'GET') && (!empty($_GET['dineroInicial'])) )
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset ($_POST['accion'])) {
         if ($_POST['accion'] == 'apostar') {
-            $numCasino = random_int(1, 10);
-            if ($numCasino % 2 == 0) {
-                $resultadoCasino = "par";
-            } else {
-                $resultadoCasino = "impar";
+
+            if ($_POST['dineroApuesta'] > $_SESSION['dinero']) {
+                $msg = "No puede apostar más dinero del que tiene.";
+                include 'apuesta.php';
             }
-        
-            if (isset($_POST['apuesta'])) {
-                if ($_POST['apuesta'] == $resultadoCasino) {
-                    $msg = "RESULTADO DE LA APUESTA: " . $resultadoCasino . "<br> ¡HA GANADO!";
-                    if (is_numeric($_POST['dineroApuesta'])) {
-                        $_SESSION['dinero'] += $_POST['dineroApuesta'];
-                        
-                    } else { $msg = "Debe introducir un importe para apostar."; }
-                    include 'apuesta.php';
+            else {
+                $numCasino = random_int(1, 10);
+                if ($numCasino % 2 == 0) {
+                    $resultadoCasino = "par";
                 } else {
-                    $msg = "RESULTADO DE LA APUESTA: " . $resultadoCasino . "<br> HA PERDIDO :(";
-                    if (is_numeric($_POST['dineroApuesta'])) {
-                        $_SESSION['dinero'] -= $_POST['dineroApuesta'];
-                    } else { $msg = "Debe introducir un importe para apostar."; }
-                    include 'apuesta.php';
+                    $resultadoCasino = "impar";
+                }
+            
+                if (isset($_POST['apuesta'])) {
+                    if ($_POST['apuesta'] == $resultadoCasino) {
+                        $msg = "RESULTADO DE LA APUESTA: " . $resultadoCasino . "<br> ¡HA GANADO!";
+                        if (is_numeric($_POST['dineroApuesta'])) {
+                            $_SESSION['dinero'] += $_POST['dineroApuesta'];
+                            
+                        } else { $msg = "Debe introducir un importe para apostar."; }
+                        include 'apuesta.php';
+                    } else {
+                        $msg = "RESULTADO DE LA APUESTA: " . $resultadoCasino . "<br> HA PERDIDO :(";
+                        if (is_numeric($_POST['dineroApuesta'])) {
+                            $_SESSION['dinero'] -= $_POST['dineroApuesta'];
+                            if ($_SESSION['dinero'] == 0) {
+                                include 'final.php';
+                                $veces++;
+                                setcookie('veces', $veces, time() + 60*60*24*30);
+                            } else { include 'apuesta.php'; }
+                        } else { $msg = "Debe introducir un importe para apostar."; include 'apuesta.php';}
+                    }
                 }
             }
+
+            
         } else {
             include 'final.php';
             $veces++;
